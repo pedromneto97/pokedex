ab522262d676162dc359de3b75f654c9c595eb16
Pedro Neto <pedromneto97@gmail.com>
2021-04-30 23:44:22 -0300

    feat: Models base

---
+++ lib/models/Pokemon.dart
@@ -0,0 +1,21 @@
+class Pokemon {
+  final String name;
+  final List<String> abilities;
+  final int baseExperience;
+  final int height;
+  final String image;
+  final List<String> types;
+  final int weight;
+  final String color;
+
+  Pokemon({
+    required this.name,
+    required this.abilities,
+    required this.baseExperience,
+    required this.height,
+    required this.weight,
+    required this.image,
+    required this.types,
+    required this.color,
+  });
+}
---
+++ lib/models/PokemonColor.dart
@@ -0,0 +1,9 @@
+class PokemonColor {
+  final String name;
+  final List<String> pokemonNames;
+
+  PokemonColor({
+    required this.name,
+    required this.pokemonNames,
+  });
+}
