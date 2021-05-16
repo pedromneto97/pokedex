aa3b7ac01cc8bbbaed4fbcbf4b9da41f1600f21b
Pedro Neto <pedromneto97@gmail.com>
2021-05-16 12:42:19 -0300

    feat: Ajusta title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokedex',
       initialRoute: Pokedex.screenName,
       routes: {
         Pokedex.screenName: (context) => const Pokedex(),
