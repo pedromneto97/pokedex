32450e657ebd903f7a4a7d1834a0f1125e3ce9f5
Pedro Neto <pedromneto97@gmail.com>
2021-05-16 12:41:48 -0300

    feat: Adiciona widget de labeled text

---
+++ lib/widgets/labeled_text.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LabeledText extends StatelessWidget {
+  final String label;
+  final String text;
+
+  const LabeledText({
+    Key? key,
+    required this.label,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text(this.label),
+        ),
+        Expanded(
+          flex: 3,
+          child: Text(
+            this.text,
+            textAlign: TextAlign.left,
+          ),
+        ),
+      ],
+    );
+  }
+}
