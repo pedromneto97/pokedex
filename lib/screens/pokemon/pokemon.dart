0eb6c96d10d5c1b030ec5d42870762080794b40f
Pedro Neto <pedromneto97@gmail.com>
2021-06-05 19:38:28 -0300

    feat: Ajusta base da tela de detalhe do pokemon

--- lib/screens/pokemon/pokemon.dart
+++ lib/screens/pokemon/pokemon.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../../models/Pokemon.dart';
-import '../../widgets/labeled_text.dart';
+import '../../assets/pokemon_icons.dart';
+import '../../design_system/colors.dart';
+import '../../models/pokemon.dart';
+import '../../models/pokemon_stat.dart';
+import '../../utils/pokemon.dart';
 import '../../widgets/pokemon_type.dart';
 import 'widgets/pokemon_stat.dart';
 
@@ -12,137 +15,173 @@ class PokemonScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final color = getColorFromType(pokemon.types.first);
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
-            iconSize: 24,
-            icon: Icon(Icons.arrow_back_rounded),
-            onPressed: () {
-              Navigator.of(context).pop();
-            }),
+          iconSize: 24,
+          icon: const Icon(Icons.arrow_back_rounded),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text(
+          pokemon.name,
+          style: const TextStyle(
+            fontSize: 24,
+            height: 1.333333333,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Center(
+              child: Text(
+                pokemon.idHash,
+                style: const TextStyle(
+                  fontSize: 12,
+                  height: 2.666666667,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
       body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          stops: [
-            0.05,
-            1,
-          ],
-          colors: [
-            pokemon.color.primaryColor,
-            pokemon.color.secondaryColor,
-          ],
-        )),
+        color: color,
         child: Stack(
           children: [
             Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
+                borderRadius: BorderRadius.circular(8),
                 color: Colors.white,
               ),
-              margin:
-                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
-              padding: EdgeInsets.symmetric(vertical: 40, horizontal: 16),
+              margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 3,
+                left: 4,
+                right: 4,
+                bottom: 4,
+              ),
+              padding: const EdgeInsets.only(
+                top: 56,
+                left: 16,
+                right: 16,
+                bottom: 44,
+              ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Hero(
-                    tag: Key("${pokemon.name}-Name"),
-                    child: Text(
-                      pokemon.name,
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.w900,
-                        height: 1.25,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: pokemon.types.asMap().entries.map<Widget>((e) {
                       if (e.key == 0) {
-                        return Padding(
-                          key: ObjectKey(e),
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: PokemonType(
-                            pokemonType: e.value,
-                            color: pokemon.color.primaryColor,
-                          ),
-                        );
-                      }
-                      return Hero(
-                        tag: Key("${pokemon.name}-${e.value}"),
-                        child: PokemonType(
+                        return PokemonType(
                           key: ObjectKey(e),
                           pokemonType: e.value,
-                          color: pokemon.color.primaryColor,
+                        );
+                      }
+                      return Padding(
+                        key: ObjectKey(e),
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: PokemonType(
+                          pokemonType: e.value,
                         ),
                       );
                     }).toList(),
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8),
-                    child: LabeledText(
-                      label: "Height",
-                      text: "${pokemon.height / 10} m",
+                    child: Text(
+                      'About',
+                      style: TextStyle(
+                        fontSize: 14,
+                        height: 1.142857143,
+                        fontWeight: FontWeight.bold,
+                        color: color,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  LabeledText(
-                    label: "Weight",
-                    text: "${pokemon.weight / 100} m",
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: LabeledText(
-                      label: "Abilities",
-                      text: pokemon.abilities.join(", "),
-                    ),
-                  ),
-                  DefaultTabController(
-                    length: 1,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                  Container(
+                    height: 48,
+                    child: Row(
                       children: [
-                        Container(
-                          child: TabBar(
-                            labelColor: pokemon.color.primaryColor,
-                            indicatorColor: pokemon.color.primaryColor,
-                            tabs: [
-                              Tab(
-                                text: "Stats",
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          height: 200,
-                          child: TabBarView(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Column(
-                                  children: pokemon.stats
-                                      .map(
-                                        (e) => PokemonStat(
-                                          stat: e,
-                                          color: pokemon.color.primaryColor,
-                                          backgroundColor:
-                                              pokemon.color.secondaryColor,
-                                        ),
-                                      )
-                                      .toList(),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  PokemonIcons.weight,
+                                  size: 16,
+                                  color: darkGray,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text(
+                                    '${69 / 100} kg',
+                                    style: const TextStyle(
+                                      height: 1.6,
+                                      fontSize: 10,
+                                      color: darkGray,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 8.0),
+                              child: Text(
+                                'Weight',
+                                style: TextStyle(
+                                  color: mediumGray,
+                                  height: 1.5,
+                                  fontSize: 8,
                                 ),
                               ),
-                            ],
-                          ),
-                        )
+                            ),
+                          ],
+                        ),
                       ],
                     ),
+                  ),
+                  const Text(
+                    'There is a plant seed on its back right from the day this '
+                    'Pokémon is born. The seed slowly grows larger.',
+                    style: TextStyle(
+                      fontSize: 10,
+                      height: 1.6,
+                      color: darkGray,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Text(
+                      'Base Stats',
+                      style: TextStyle(
+                        fontSize: 14,
+                        height: 1.142857143,
+                        fontWeight: FontWeight.bold,
+                        color: color,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Column(
+                    children: [Stat(name: 'ATK', value: 50)]
+                        .map(
+                          (e) => PokemonStat(
+                            stat: e,
+                            color: color,
+                            backgroundColor: color.withOpacity(0.2),
+                          ),
+                        )
+                        .toList(),
                   )
                 ],
               ),
