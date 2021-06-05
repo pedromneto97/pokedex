c9695d6eb276ab0421e8e14b0444f5f3b7c1871d
Pedro Neto <pedromneto97@gmail.com>
2021-06-05 19:36:46 -0300

    feat: Ajusta stats

--- lib/screens/pokemon/widgets/pokemon_stat.dart
+++ lib/screens/pokemon/widgets/pokemon_stat.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/design_system/colors.dart';
 
-import '../../../models/PokemonStat.dart';
+import '../../../models/pokemon_stat.dart';
 
 class PokemonStat extends StatelessWidget {
   final Stat stat;
@@ -19,37 +20,47 @@ class PokemonStat extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Expanded(
+        Container(
+          width: 32,
           child: Text(
             stat.name,
             style: TextStyle(
               color: color,
-              height: 1.25,
-              fontSize: 12,
-              fontWeight: FontWeight.w900,
+              height: 1.6,
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
             ),
+            textAlign: TextAlign.right,
           ),
         ),
-        Expanded(
+        Container(
+          height: 16,
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: const VerticalDivider(
+            color: lightGray,
+            width: 1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 8.0),
           child: Text(
             stat.value.toString().padLeft(3, '0'),
-            style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 14,
-              height: 1.25,
-              color: Colors.black54,
+            style: const TextStyle(
+              fontSize: 10,
+              height: 1.6,
+              color: darkGray,
             ),
           ),
         ),
         Expanded(
-          flex: 4,
+          flex: 10,
           child: ClipRRect(
             borderRadius: BorderRadius.circular(4),
             child: LinearProgressIndicator(
               valueColor: AlwaysStoppedAnimation<Color>(color),
               backgroundColor: backgroundColor,
               value: stat.value / 255,
-              minHeight: 8,
+              minHeight: 4,
             ),
           ),
         ),
