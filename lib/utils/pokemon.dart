25d31883a59581b8e8f15aadf1ce2a4fadf4058c
Pedro Neto <pedromneto97@gmail.com>
2021-06-05 17:22:07 -0300

    feat: Ajusta utils para pegar cor e map do pokemon

--- lib/utils/pokemon.dart
+++ lib/utils/pokemon.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/widgets.dart';
+
 import '../models/Pokemon.dart';
 import '../models/PokemonColor.dart';
 import '../models/PokemonStat.dart';
@@ -67,6 +69,7 @@ Map<String, dynamic> mapPokemon(Map<String, dynamic> data) {
         .toList();
     pokemons.add(
       Pokemon(
+        id: element['id'],
         name: name.capitalize(),
         abilities: abilities,
         baseExperience: element['base_experience'],
@@ -75,7 +78,6 @@ Map<String, dynamic> mapPokemon(Map<String, dynamic> data) {
         image:
             'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${element['id']}.png',
         types: types,
-        color: color,
         stats: stats,
       ),
     );
@@ -92,4 +94,28 @@ extension StringExtension on String {
   String capitalize() {
     return "${this[0].toUpperCase()}${this.substring(1)}";
   }
+}
+
+Color getColorFromType(String type) {
+  const colors = {
+    'rock': Color(0xFFB69E31),
+    'ghost': Color(0xFF70559B),
+    'steel': Color(0xFFB7B9D0),
+    'water': Color(0xFF74CB48),
+    'grass': Color(0xFF74CB48),
+    'psychic': Color(0xFFFB5584),
+    'ice': Color(0xFF9AD6DF),
+    'dark': Color(0xFF75574C),
+    'fairy': Color(0xFFE69EAC),
+    'normal': Color(0xFFAAA67F),
+    'fighting': Color(0xFFC12239),
+    'flying': Color(0xFFA891EC),
+    'poison': Color(0xFFA43E9E),
+    'ground': Color(0xFFDEC16B),
+    'bug': Color(0xFFA7B723),
+    'fire': Color(0xFFF57D31),
+    'electric': Color(0xFFF9CF30),
+    'dragon': Color(0xFF7037FF),
+  };
+  return colors[type.toLowerCase()]!;
 }
