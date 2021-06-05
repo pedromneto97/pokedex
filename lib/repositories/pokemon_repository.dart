fec4ff4b0ee329ed8d6e729ebd3b723d54d86fae
Pedro Neto <pedromneto97@gmail.com>
2021-06-05 19:37:40 -0300

    feat: adiciona find do pokemon

--- lib/repositories/pokemon_repository.dart
+++ lib/repositories/pokemon_repository.dart
@@ -4,6 +4,7 @@ import '../utils/graphql.dart';
 
 class PokemonRepository {
   PokemonRepository._();
+
   static PokemonRepository? _instance;
   final int pageSize = 20;
 
@@ -50,6 +51,47 @@ class PokemonRepository {
         variables: {
           'page': (page - 1) * pageSize,
           'pageSize': pageSize,
+        },
+      ),
+    )
+        .then((value) {
+      if (value.hasException) {
+        throw value.exception as OperationException;
+      }
+      if (value.data == null) {
+        throw Exception('Missing data');
+      }
+      return value.data as Map<String, dynamic>;
+    });
+    return data;
+  }
+
+  Future<Map<String, dynamic>> findPokemon({
+    required int id,
+  }) async {
+    final data = await client
+        .query(
+      QueryOptions(
+        document: gql('''
+            query QueryPokemons(\$id: Int!) {
+              pokemon: pokemon_v2_pokemon_by_pk(id: \$id) {
+                height
+                abilities: pokemon_v2_pokemonabilities {
+                  ability: pokemon_v2_ability {
+                    name
+                  }
+                }
+                weight
+                specy: pokemon_v2_pokemonspecy {
+                  flavor: pokemon_v2_pokemonspeciesflavortexts(limit: 1) {
+                    flavor_text
+                  }
+                }
+              }
+          }
+      '''),
+        variables: {
+          'id': id,
         },
       ),
     )
