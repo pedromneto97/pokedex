0b9dca3dd3e10bffc8a5e62ce5952c4c1024b6a7
Pedro Neto <pedromneto97@gmail.com>
2021-06-05 19:37:52 -0300

    feat: separa as classes em duas

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -6,25 +6,40 @@ import 'pokemon_stat.dart';
 class Pokemon {
   final int id;
   final String name;
-  final List<String> abilities;
-  final int baseExperience;
-  final int height;
   final String image;
   final List<String> types;
-  final int weight;
-  final List<Stat> stats;
 
   const Pokemon({
     required this.id,
     required this.name,
-    required this.abilities,
-    required this.baseExperience,
-    required this.height,
-    required this.weight,
     required this.image,
     required this.types,
-    required this.stats,
   });
 
   String get idHash => '#${id.toString().padLeft(3, '0')}';
+}
+
+@immutable
+class DetailedPokemon extends Pokemon {
+  final int weight;
+  final List<Stat> stats;
+  final int baseExperience;
+  final int height;
+  final List<String> abilities;
+  final String about;
+
+  DetailedPokemon.fromPokemon({
+    required Pokemon pokemon,
+    required this.weight,
+    required this.stats,
+    required this.baseExperience,
+    required this.height,
+    required this.abilities,
+    required this.about,
+  }) : super(
+          id: pokemon.id,
+          name: pokemon.name,
+          image: pokemon.image,
+          types: pokemon.types,
+        );
 }
