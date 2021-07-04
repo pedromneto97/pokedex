418f454dc9254c457cf91d51e20277b4a43955ed
Pedro Neto <pedromneto97@gmail.com>
2021-07-03 22:09:35 -0300

    feat: cor primária

--- lib/design_system/design_system.dart
+++ lib/design_system/design_system.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'colors.dart';
+
 final theme = ThemeData(
+  primaryColor: darkGray,
   brightness: Brightness.light,
   fontFamily: 'Poppins',
 );
